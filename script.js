7cbc21966a315314785169657fcad50a37415257
suvadip majhi <114084926+suvadipmajhi1997@users.noreply.github.com>
2025-03-22 21:34:53 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.slider-wrapper', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+    autoPlay: {
+        speed: 5000,
+      },
+    
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        }
+    }
+  });
